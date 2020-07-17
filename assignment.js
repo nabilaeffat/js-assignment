3bff684b3eb21e13e62a684870d978292aebbfec
Syeda Nabila Effath <s.nabila.effat@gmail.com>
2020-07-17 16:52:01 +1000

    update

--- assignment.js
+++ assignment.js
@@ -1,5 +1,5 @@
 
-// feetToMile: convert feet to mile.   
+//1 feetToMile: convert feet to mile.   
 function feetToMile(userInput){
     if (Number.isInteger(userInput) && userInput >0){
 
@@ -15,33 +15,44 @@ function feetToMile(userInput){
     }
 }
 
-//console.log(feetToMile(5));
+//console.log(feetToMile(-5));
 
-//woodCalculator: user inputs are number of chairs, tables and beds. 
-// 1 chair = 1 cubic feet wood
-// 1 table = 3 cubic feet wood
-// 1 bed = 5 cubic feet wood 
+/** 2 woodCalculator: user inputs are number of chairs, tables and beds. 
+ 1 chair = 1 cubic feet wood
+ 1 table = 3 cubic feet wood
+ 1 bed = 5 cubic feet wood 
+*/ 
 function woodCalculator(chair, table, bed){
 
-    if (Number.isInteger(chair) && chair >0){
+    if (!Number.isInteger(chair) || chair <0 ){
+        
+        return "Invalid user input, chair must be positive number.";
 
-        var totalWood = chair*1 + table*3 + bed*5;
-        var result = "You need total "+totalWood +" cubic feet wood.";
-        return result;
+    }
+    else if(!Number.isInteger(table) || table <0){
+
+        return "Invalid user input, table must be positive number.";
+    }
+    else if(!Number.isInteger(bed) || bed <0){
+
+        return "Invalid user input, chair must be positive number.";
     }
     else
     {
-        return "Invalid user input, it must be positive number.";
+        var totalWood = chair*1 + table*3 + bed*5;
+        var result = "You need total "+totalWood +" cubic feet wood.";
+        return result;
     }
 
 }
 
 //console.log(woodCalculator(12,1,2));
 
-/** brickCalculator: Calculate to
-    1 foot = 1000 brick
-    1st to 10th floor height= 15 fee;
-    11th to 20th  =12 feet; 21 to 30 = 10 feet
+/** 3 brickCalculator: 
+    1 foot = 1000 bricks
+    1st to 10th floor height= 15 feet;
+    11th to 20th  =12 feet; 
+    21th to rest = 10 feet
     user input = floor number
 */
 
@@ -55,7 +66,6 @@ function brickCalculator(floorNo){
     var restult = "Invalid Input";
     if (Number.isInteger(floorNo) && floorNo > 0){
         
-
         switch (true) {
         case floorNo <=10:
             totalHeight = floorNo*floorHeight10th;
@@ -78,29 +88,33 @@ function brickCalculator(floorNo){
     return restult;
 }
 
-console.log(brickCalculator(11));
+//console.log(brickCalculator(11));
 
 //4.tinyFriends: find Shortest name in the array.
 
 function tinyFriend(friends){
 
-   if (friends.length != 0){
-       var tinyName = friends[0];
-       for (var i = 0; i < friends.length; i++){   
-           if(friends[i].length < tinyName.length ){
-            tinyName = friends[i];
-           }
+   if (friends.length != 0 ){
+    var tinyName = friends[0];
+
+       if(!Number.isInteger(tinyName)){
+        for (var i = 0; i < friends.length; i++){   
+            if(friends[i].length < tinyName.length ){
+             tinyName = friends[i];
+            }
+        }
+        return tinyName +" is my tiny friend.";
        }
+       else {
+           return "Friends name array must be string array.";
+       }
+
     
-    return tinyName +" is my tiny friend.";
    }
    else {  
     return "Your friends array is empty";
    }
  }
 
- //var result = tinyFriend(['Nabila','Shahzadi', 'Mou','Nipa']);
- //var result2 = tinyFriend(['Shahzadi','Nabila', 'Mou','Nipa']);
- //var result3 = tinyFriend(['Mou','Shahzadi','Nabila', 'Nipa']);
- //var result1 = tinyFriend([]);
-//console.log(result3);
+//var result = tinyFriend(['Nabila','Shahzadi', 'Mou','Nipa']);
+//console.log(result);
